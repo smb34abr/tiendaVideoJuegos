0c37ef1b55671e97497d616d0a3023af8b1a5b9a
Alberto Bernal <smb34abr@gmail.com>
2025-12-22 15:55:23 -0600

    No recuerdo que realice

--- respuesta.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-# Hacer una solicitud GET a una API de ejemplo
-response = requests.get('https://jsonplaceholder.typicode.com/posts/1', timeout=5)
-
-# Imprimir la respuesta
-print(response.status_code)
-print(response.json())
